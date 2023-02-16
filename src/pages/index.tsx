94bd9948f7fb08add4d82c0469b7f64c94fd4428
Jonas Janssens <jansseskes@live.be>
2023-02-16 20:54:29 +0100

    first changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,26 +17,10 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Jonas Janssens portfolio&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+          
         </div>
 
         <div className={styles.center}>
